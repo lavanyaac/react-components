3b86825ccb238c699db511c094a70ff5b4fd4606
Lavanya Ammi Chandrashekara <lavanya.ammic@gmail.com>
2017-04-15 17:14:09 -0700

    completed

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'none'
+    };
+
+    return ( < li style = { style }
+      onMouseOver = { this.onListItemClick.bind(this) } > { this.props.todo } < /li>
+    );
+  }
+}
+
+
+var GroceryList = (props) =>
+  ( < ul > {
+      props.todos.map(todo =>
+        < GroceryListItem todo = { todo }
+        />
+      )
+    } < /ul>
+
+  );
+
+
+var App = () => ( < div >
+< h2 > Hello There < /h2>  < GroceryList todos = { ['Learn React', 'Crush React.ly', 'Maybe Sleep']
+}
+/ > < /div > );
+
+ReactDOM.render( < App / > , document.getElementById("app"));
